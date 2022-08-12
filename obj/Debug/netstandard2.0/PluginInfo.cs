53f0ed44b30429348af1ec94ccfca09a5fa8a99b
ZedDevStuff <98429642+ZedDevStuff@users.noreply.github.com>
2022-08-12 13:24:31 +0000

    Delete obj directory

--- obj/Debug/netstandard2.0/PluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace UKUIHelper
-{
-    public static class PluginInfo
-    {
-        public const string PLUGIN_GUID = "UKUIHelper";
-        public const string PLUGIN_NAME = "UKUIHelper";
-        public const string PLUGIN_VERSION = "1.0.0";
-    }
-}
